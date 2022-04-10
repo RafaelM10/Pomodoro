d801b0297c214819f353d16ec3900ef624e094aa
RafaelM10 <rafameira10@icloud.com>
2022-04-10 17:50:39 -0300

    configurando Rest/secondsToTime

--- src/utils/secondsToTime.tsx
+++ src/utils/secondsToTime.tsx
@@ -2,5 +2,5 @@ export function secondsToTime(seconds: number): string {
     const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0')
     const min = zeroLeft ((seconds / 60 ) % 60);
     const sec = zeroLeft((seconds % 60) % 60);
-    return `${min}:00`;
+    return `${min}:${sec}`;
 }
